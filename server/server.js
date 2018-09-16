f53857d40e7b1b9c003a5ded5ce84f4a9df51d91
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-09-16 14:43:04 -0300

    Ultima antes do Firebase

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3003;
 
 app.use(express.static(publicPath));
 
