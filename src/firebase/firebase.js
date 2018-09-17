287b73ee20cef31e25cb1cf084a350f201e8e865
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-09-17 12:57:50 -0300

    Database Direct Addressing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/auth';
 import 'firebase/messaging';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: 'AIzaSyBnjWQ_FqSIljCLISidpS9ZbZgftfMhOwE',
+  authDomain: 'contador-de-gastos.firebaseapp.com',
+  databaseURL: 'https://contador-de-gastos.firebaseio.com',
+  projectId: 'contador-de-gastos',
+  storageBucket: 'contador-de-gastos.appspot.com',
+  messagingSenderId: '270712574007'
 };
 
 firebase.initializeApp(config);
