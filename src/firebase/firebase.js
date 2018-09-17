0002ed80ad05dae7891bcc0cb8c3f3a34689ba4f
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-09-17 12:52:48 -0300

    Database Test Suits L155

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,19 +5,19 @@ import 'firebase/auth';
 import 'firebase/messaging';
 
 const config = {
-  apiKey: 'AIzaSyBnjWQ_FqSIljCLISidpS9ZbZgftfMhOwE',
-  authDomain: 'contador-de-gastos.firebaseapp.com',
-  databaseURL: 'https://contador-de-gastos.firebaseio.com',
-  projectId: 'contador-de-gastos',
-  storageBucket: 'contador-de-gastos.appspot.com',
-  messagingSenderId: '270712574007'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
-const messaging = firebase.messaging();
 const auth = firebase.auth();
+// const messaging = firebase.messaging();
 
 export { firebase, database as default };
 
